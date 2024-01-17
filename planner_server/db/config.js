3f57bc5e8aa89e332ba5f697c7ed121f07382e3d
yar-0h <simmonsy02@gmail.com>
2024-01-17 01:29:48 -0500

    restructured to monorepo with yarn

---
+++ planner_server/db/config.js
@@ -0,0 +1,15 @@
+// constains config info, ie. database credentials, etc
+
+const config = {
+
+    db: {
+        // dont expose this stuff (obviously)
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "foldingInward",
+        database: "planner_db",
+    },
+};
+
+module.exports = config;
